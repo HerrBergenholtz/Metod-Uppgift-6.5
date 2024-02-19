3bd54693da5dee340f3b2eafdbde4580bd2de5df
HerrBergenholtz <zbdv8800@gmail.com>
2024-02-19 21:28:35 +0100

    Add project files.

---
+++ Metod-Uppgift-6.5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Uppgift_6_5
+{
+    class Program
+    {
+        static void Main()
+        {
+            string test1 = "Hejsan";
+            string test2 = "Hejsansvejsan";
+
+            Console.WriteLine(Längst(test1, test2));
+
+            int tal1 = 10;
+            int tal2 = 20;
+
+            Console.WriteLine(Längst(tal1, tal2));
+        }
+
+        static int Längst(int tal1, int tal2)
+        {
+            if (tal1 >= tal2) return tal1;
+            else return tal2;
+        }
+
+        static string Längst(string sträng1, string sträng2)
+        {
+            if (sträng1.Length >= sträng2.Length) return sträng1;
+            else return sträng2;
+        }
+    }
+}
